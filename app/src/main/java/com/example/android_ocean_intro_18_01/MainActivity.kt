162c1ff3e398142931f86ffa17de8830f84eaa04
Danilo-Miguel <danilo.miguel@oceanbrasil.com>
2024-01-18 12:02:29 -0300

    Criação da variável para armazenar e manipular o botão

--- app/src/main/java/com/example/android_ocean_intro_18_01/MainActivity.kt
+++ app/src/main/java/com/example/android_ocean_intro_18_01/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.android_ocean_intro_18_01
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnEnviar = findViewById<Button>(R.id.btn_enviar)
+       
     }
 }
