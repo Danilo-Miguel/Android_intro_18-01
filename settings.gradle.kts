bec3f46047470d7e0aa572079893ecabfa62078a
Danilo-Miguel <danilo.miguel@oceanbrasil.com>
2024-01-18 11:46:22 -0300

    Criação do projeto Android

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Android_Ocean_Intro_18-01"
+include(":app")
+ 
