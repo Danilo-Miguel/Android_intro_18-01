bb3aa99203e44407e6ae2810f9ec756c10c4c0a6
Danilo-Miguel <danilo.miguel@oceanbrasil.com>
2024-01-19 11:36:10 -0300

    Criação do botão voltar na activity result

--- app/src/main/java/com/example/android_ocean_intro_18_01/Result.kt
+++ app/src/main/java/com/example/android_ocean_intro_18_01/Result.kt
@@ -1,11 +1,20 @@
 package com.example.android_ocean_intro_18_01
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Result : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val btnVoltar = findViewById<Button>(R.id.btn_voltar)
+
+        btnVoltar.setOnClickListener {
+            finish()
+        }
     }
 }
