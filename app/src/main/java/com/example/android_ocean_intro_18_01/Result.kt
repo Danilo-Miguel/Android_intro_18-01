09c0df709ec671072a64d5c966ef845547578dbc
Danilo-Miguel <danilo.miguel@oceanbrasil.com>
2024-01-19 11:06:19 -0300

    Criação da Activity result

---
+++ app/src/main/java/com/example/android_ocean_intro_18_01/Result.kt
@@ -0,0 +1,11 @@
+package com.example.android_ocean_intro_18_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Result : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
