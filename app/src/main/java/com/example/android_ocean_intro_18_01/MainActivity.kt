4838fe7207181c8631b8e3e6abe5a007bb7574ff
Danilo-Miguel <danilo.miguel@oceanbrasil.com>
2024-01-19 11:22:00 -0300

    Criar botão para abrir nova tela

--- app/src/main/java/com/example/android_ocean_intro_18_01/MainActivity.kt
+++ app/src/main/java/com/example/android_ocean_intro_18_01/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.android_ocean_intro_18_01
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         val btnEnviar = findViewById<Button>(R.id.btn_enviar)
         val txtOne =  findViewById<TextView>(R.id.txt_one)
         val editTxt = findViewById<EditText>(R.id.edit_txt)
+        val btnAbrirTela =  findViewById<Button>(R.id.btn_abrir_tela)
 
         btnEnviar.setOnClickListener {
             val input = editTxt.text
@@ -25,8 +27,15 @@ class MainActivity : AppCompatActivity() {
             }else {
                 txtOne.text = editTxt.text
             }
+        }
+
+        btnAbrirTela.setOnClickListener {
+            val abrirNovaTela = Intent(this, Result::class.java)
+            startActivity(abrirNovaTela)
 
         }
+
+
 
 
 
