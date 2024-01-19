7b148f054bc09037e869995b34b54e1a10c218c9
Danilo-Miguel <danilo.miguel@oceanbrasil.com>
2024-01-19 09:17:42 -0300

    Criação das variáveis para o botão e para a TextView

--- app/src/main/java/com/example/android_ocean_intro_18_01/MainActivity.kt
+++ app/src/main/java/com/example/android_ocean_intro_18_01/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnEnviar = findViewById<Button>(R.id.btn_enviar)
-       
+        val txtOne =  findViewById<TextView>(R.id.txt_one)
+
+
+
     }
 }
